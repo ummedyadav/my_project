f5ed533d46e01f127594e7df0df267a554a2ffab
mukesh <yadavummedsingh@gmail.com>
2019-11-10 23:30:12 +0530

    first push

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div id="app">
+  	<input v-model="message" class="form-control">
+  <span v-if="seen" v-bind:title="time">
+    {{ message }}
+  </span>
+</div>
+</template>
+
+<script type="text/javascript">
+	export default {
+  		data(){
+    		return{ 
+    				message:'testing',
+    				time:'You loaded this page on ' + new Date().toLocaleString(),
+    				seen:true
+    			}
+    }
+  }
+  		
+</script>
